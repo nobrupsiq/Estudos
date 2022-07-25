ba62cc964bc691085ef28042a7784a7d3cac18bc
bruno <bpires.siqueira@gmail.com>
2022-07-25 15:39:38 -0300

    final classes e atributos

---
+++ JavaScript/dom/classes-e-atributos/exercicio.js
@@ -0,0 +1,35 @@
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item) => {
+  item.classList.add('ativo');
+  console.log(item);
+});
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+  item.classList.remove('ativo');
+  console.log(item);
+});
+itensMenu[0].classList.add('ativo');
+
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+  const possuiAtributo = img.hasAttribute('alt');
+  console.log(img, possuiAtributo);
+});
+
+// Modifique o href do link externo no menu
+
+// const linkExterno = itensMenu[itensMenu.length -1];
+// linkExterno.setAttribute('href', 'oi');
+// console.log(linkExterno);
+
+// Outra forma
+
+const linkExterno2 = document.querySelector('a[href^="http"]');
+linkExterno2.setAttribute('href', 'https://www.google.com.br/');
+console.log(linkExterno2);
+
--- JavaScript/dom/classes-e-atributos/script-aula.js
+++ JavaScript/dom/classes-e-atributos/script-aula.js
@@ -15,3 +15,51 @@ menu.classList.replace('ativo', 'inativo'); // substitui
 if(menu.classList.contains('inativo')) {
   console.log('Possui a classe inativo');
 }
+
+menu.className += ' vermelho';
+console.log(menu.className)
+
+
+// ATTRIBUTES
+// Retorna uma array-like com os atributos do elemento.
+
+const animais = document.querySelector('.animais');
+console.log(animais.attributes['data-texto']);
+
+animais.attributes; // retorna todos os atributos
+animais.attributes[0]; // retorna o primeiro atributo
+
+
+// GETATTRIBUTE E SETATTRIBUTE
+// Métodos que retornam ou definem de acordo com o atributo selecionado
+
+const img = document.querySelector('img');
+
+img.getAttribute('src'); // valor do src
+img.setAttribute('alt', 'Texto Alternativo'); // muda o alt
+img.hasAttribute('id'); // true / false
+img.removeAttribute('alt'); // remove o alt
+
+// img.hasAttribute(); // true / false se tem algum atributo
+
+// É muito comum métodos de get e set
+
+
+
+// READ ONLY VS WRITABLE
+// Existem propriedades que não permitem a mudança de seus valores, essas são considerados Read Only, ou seja, apenas leitura.
+
+const animais2 = document.querySelector('.animais');
+
+animais.className; // string com o nome das classes
+animais.className = 'Azul'; // substitui completamente a string
+animais.className += ' vermelho'; // adiciona vermelho à string
+
+animais.attributes = 'class="ativo"'; // não funciona, read-only
+
+const carro = {
+  portas: 4,
+  andar: function(km) {
+    console.log(`Andou ${km}KM`);
+  }
+}

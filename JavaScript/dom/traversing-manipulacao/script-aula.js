6adf36931a16e764d89d2cc549682bb9a5cde89f
bruno <bpires.siqueira@gmail.com>
2022-07-28 15:30:49 -0300

    aula

--- JavaScript/dom/traversing-manipulacao/script-aula.js
+++ JavaScript/dom/traversing-manipulacao/script-aula.js
@@ -6,7 +6,7 @@ const animaisLista = document.querySelector('.animais-descricao');
 // console.log(h1.innerHTML) // retorna apenas o texto do meu h1
 // console.log(h1.outerHTML) // pega tudo de fora
 
-h1.outerHTML = '<p>Novo Título</p>';
+// h1.outerHTML = '<p>Novo Título</p>';
 // console.log(animaisLista.innerText);
 
 
@@ -41,4 +41,49 @@ lista2.previousSibling; // node acima
 lista2.fisrtChild; // primeiro node child
 lista2.childNodes; // todos os node child
 
-console.log(lista2.previousSibling)
+// console.log(lista2.previousSibling)
+
+
+
+// MANIPULANDO ELEMENTOS
+// É possível mover elementos no dom com métodos de Node.
+const animais = document.querySelector('.animais');
+const contato = document.querySelector('.contato');
+const titulo = contato.querySelector('.titulo');
+const mapa = document.querySelector('.mapa');
+
+
+
+// contato.appendChild(lista); // move lista para o final de contato
+// contato.insertBefore(lista, titulo); // insere a lista antes do titulo
+// contato.removeChild(titulo); // remove titulo de contato
+// contato.replaceChild(lista, titulo); // subistitui titulo por lista
+
+
+
+
+// NOVOS ELEMENTOS
+// Podemos criar novos elementos com o método createElement()
+
+const animais2 = document.querySelector('.animais');
+
+const novoH1 = document.createElement('h1');
+novoH1.innerText = 'Novo Título';
+novoH1.classList.add('titulo');
+
+animais2.appendChild(novoH1);
+
+
+
+// CLONAR ELEMENTOS
+// Todo elemento selecionado é único. Para criarmos um novo elemento baseado no anterior, é necessário utilizar o método cloneNode()
+
+
+const titulo3 = document.querySelector('h1');
+const titulo4 = document.querySelector('h1');
+const novoTitulo = titulo3;
+// titulo3, titulo4 e novoTitulo são iguais
+
+const cloneTitulo = titulo3.cloneNode(true);  // true sinaliza para incluir os filhos do elemento
+const contato2 = document.querySelector('.contato');
+contato2.appendChild(cloneTitulo);

902d68f5563036badd4cda4e5bd013c3c0e40232
bruno <bpires.siqueira@gmail.com>
2022-08-07 19:45:38 -0300

    native host e user

---
+++ JavaScript/objetos/native-host-e-user/exercicio.js
@@ -0,0 +1,24 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+
+// Liste 5 objetos do browser
+window
+Document
+HTMLCollection
+NodeList
+history
+
+
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitVisibilityState !== 'undefined') {
+  console.log('existe')
+} else {
+  console.log('Não existe')
+}

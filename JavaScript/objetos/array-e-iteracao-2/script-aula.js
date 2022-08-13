a820d3abda79fda79ac620fff71b1628f793d060
bruno <bpires.siqueira@gmail.com>
2022-08-12 23:50:46 -0300

    Array e iterancao 2

--- JavaScript/objetos/array-e-iteracao-2/script-aula.js
+++ JavaScript/objetos/array-e-iteracao-2/script-aula.js
@@ -75,3 +75,16 @@ const temUva = frutas2.some((i) => {
 });
 
 
+// [].EVERY()
+// [].every(), se todos os returns das iterações forem truthy, o método irá retornar true. Se pelo menos um for falsy, ele irá retornar false.
+
+const frutas3 = ['Banana', 'Pêra', 'Uva', ''];
+// False pois pelo menos uma fruta
+// está vazia '', o que é um valor falsy
+const arraysCheias = frutas3.every((fruta) => {
+  return fruta; // false
+});
+
+const numeros = [6, 43, 22, 88, 101, 29];
+const maiorQue3 = numeros.every(n => n > 3); // true
+

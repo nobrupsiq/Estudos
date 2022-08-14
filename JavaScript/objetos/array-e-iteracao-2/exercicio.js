c3e84007240bcfa1356365d4a96ed1d2dfa8235c
bruno <bpires.siqueira@gmail.com>
2022-08-13 23:55:40 -0300

    arrayIteracao exercicio

---
+++ JavaScript/objetos/array-e-iteracao-2/exercicio.js
@@ -0,0 +1,66 @@
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
+const arrayCursos = Array.from(cursos)
+
+const objetoCurso = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector('h1').innerText
+  const descricao = curso.querySelector('p').innerText
+  const aulas = curso.querySelector('.aulas').innerText
+  const horas = curso.querySelector('.horas').innerText
+  
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  }
+})
+console.log(objetoCurso)
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+
+const maioresQue100 = numeros.filter(x => x > 100);
+console.log(maioresQue100)
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const verificarItem = instrumentos.some((item) => {
+  return item === 'Baixo';
+})
+console.log(verificarItem);
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+let somador = 0
+const valorTotal = compras.reduce((acumulador, item) => {
+  const limpador = +item.preco.replace('R$', '').trim().replace(',', '.');
+  return acumulador + limpador
+}, 0)
+
+console.log(valorTotal)

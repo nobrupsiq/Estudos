077fbd8dfc1f8a9701a286623fea02fe28128877
bruno <bpires.siqueira@gmail.com>
2022-08-18 12:25:04 -0300

    exercicio function

--- JavaScript/objetos/object-function/exercicio.js
+++ JavaScript/objetos/object-function/exercicio.js
@@ -1,14 +1,10 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
-const p = document.querySelectorAll('section p');
-const pArray = Array.from(p)
-const pMap = pArray.map((item) => {
-  return item.innerText.length;
-})
-const totalCaracter = pMap.reduce((ct, item) => {
-  return ct + item;
-})
-console.log(totalCaracter)
+const paragrafos = document.querySelectorAll('p');
+
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (ac, item) => {
+  return ac + item.innerText.length
+}, 0)
 
 
 // Crie uma função que retorne novos elementos
@@ -16,15 +12,20 @@ console.log(totalCaracter)
 // tag, classe e conteudo.
 
 function novosElementos(tag, classe, conteudo) {
-  document.createElement(tag);
-  tag.classList.add(classe);
-  tag.innerHTML = conteudo;
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHTML = conteudo : null;
+  return elemento
 }
-
-novosElementos('h1', 'ativando', 'testando');
+console.log(novosElementos('li', 'ativa', 'Bom Dia!'))
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
 
+const h1Titulo = novosElementos.bind(null, 'h1', 'titulo')
 
+const cursosJS = h1Titulo('Cursos de JavaScript');
+const cursosReact = h1Titulo('Cursos de React');
+
+console.log(cursosJS, cursosReact)

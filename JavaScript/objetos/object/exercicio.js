404a0d3798bdb6b96c8b66cb47800a13a6f9e79f
bruno <bpires.siqueira@gmail.com>
2022-08-20 11:27:26 -0300

    object exercicio

---
+++ JavaScript/objetos/object/exercicio.js
@@ -0,0 +1,32 @@
+// Crie uma função que verifique
+// corretamente o tipo de dado
+function verificarDado(dado) {
+  return Object.prototype.toString.call(dado);
+}
+console.log(verificarDado());
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+const quadrado = {}
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+    writable: false,
+  }
+})
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
+}
+Object.freeze(configuracao);
+
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+Object.getOwnPropertyNames(String.prototype);
+Object.getOwnPropertyNames(Array.prototype);

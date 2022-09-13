6c271ef54e77a43ae78c8196104522f914e37b08
bruno <bpires.siqueira@gmail.com>
2022-09-13 12:14:29 -0300

    logica javascript

---
+++ logica-js/aula9/js.js
@@ -0,0 +1,9 @@
+// Pedir para o usuário digitar dois números e mostrar a média
+
+var n1 = prompt("Digite o primeiro número: ");
+n1 = parseFloat(n1);
+
+var n2 = prompt("Digite o segundo número: ");
+n2 = parseFloat(n2);
+
+var media = (n1 + n2) / 2;

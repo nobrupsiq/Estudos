9b8ad8464757cd4c25a0f923629a5c6cb23971c5
bruno <bpires.siqueira@gmail.com>
2022-09-17 20:13:57 -0300

    alteracao aula 19

--- logica-js/aula18-19/js.js
+++ logica-js/aula18-19/js.js
@@ -3,8 +3,16 @@ var n2 = parseFloat(prompt("Digite a segunda nota: "));
 
 var media = (n1 + n2) / 2;
 
-if (media > 5 && n1 > 0 && n2 > 0) {
-   alert("Aluno aprovado!");
+// if (media > 5 && n1 > 0 && n2 > 0) {
+//    alert("Aluno aprovado!");
+// } else {
+//    alert("Aluno reprovado!");
+// }
+
+// OPERADOR OU ||
+
+if (n1 === 0 || n2 === 0 || media <= 5) {
+   alert("Aluno REPROVADO!");
 } else {
-   alert("Aluno reprovado!");
+   alert("Aluno APROVADO!");
 }

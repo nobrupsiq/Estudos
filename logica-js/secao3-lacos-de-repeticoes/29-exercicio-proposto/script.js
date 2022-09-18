6d04f2f54eaed9acfc643049f17d903d3812c94a
bruno <bpires.siqueira@gmail.com>
2022-09-18 20:28:13 -0300

    aula 29 e 30 exercicio proposto

---
+++ logica-js/secao3-lacos-de-repeticoes/29-exercicio-proposto/script.js
@@ -0,0 +1,26 @@
+// Exercicio Proposto
+// Para este exercício, mostre na tela todos os anos a partir de 1004 até o ano 2017, pulando de quatro em quatro anos.
+
+// Por exemplo:
+
+// 1004
+
+// 1008
+
+//  1012
+
+// E assim por diante.
+
+// É como mostrar os anos bissextos, mas sem as regras para descobrir se um ano é bissexto ou não. (vamos imaginar que se o ano é múltiplo de quatro, ele é bissexto).
+
+// for (let i = 1004; i <= 2017; ) {
+//    document.write(i + "<br>");
+//    i = i + 4;
+// }
+
+var ano = 1004;
+
+while (ano <= 2017) {
+   document.write(ano + "<br>");
+   ano += 4;
+}

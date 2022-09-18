006b38363d668244a253090357e86866243b9ff9
bruno <bpires.siqueira@gmail.com>
2022-09-18 00:17:16 -0300

    switch case

--- logica-js/switch-case/js.js
+++ logica-js/switch-case/js.js
@@ -1,19 +1,32 @@
-// Pedir para o usuário digitar um número de 1 a 6.
+var nome = prompt("Digite seu primeiro nome: ");
+var comprimento = nome.length;
 
-// Mostrar na tela uma mensagem diferente para cada número
-
-var n = parseInt(prompt("Digite um número: "));
-
-switch (n) {
+switch (comprimento) {
    case 1:
-      alert("boa escolha");
+      alert("oi");
       break;
    case 2:
-      alert("legal");
+      alert("ola");
       break;
    case 3:
-      alert("muito bom");
+      alert("teste");
       break;
    default:
-      alert("escolha de 1 a 3");
+      alert("algo deu errado");
+}
+
+var numero = 10;
+
+switch (typeof numero) {
+   case "number":
+      alert("é número");
+      break;
+   case "string":
+      alert("é string");
+      break;
+}
+if (typeof numero === "number") {
+   alert("é número");
+} else if (typeof numero === "string") {
+   alert("é string");
 }

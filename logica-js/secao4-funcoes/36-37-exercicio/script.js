8b0ee259386a97910c1eed82803dbac563998dcf
bruno <bpires.siqueira@gmail.com>
2022-09-19 11:48:54 -0300

    36-37 exercicio proposto

--- logica-js/secao4-funcoes/36-37-exercicio/script.js
+++ logica-js/secao4-funcoes/36-37-exercicio/script.js
@@ -1,28 +1,32 @@
-var output = document.querySelector("#output");
+var n1 = parseFloat(prompt("Digite um número: "));
 
-var mensagem = "";
+saida();
 
-function mostrarTabuada() {
-   //perguntar o numero para o usuário
-   var n = parseFloat(prompt("digite um número: "));
-   limparTabuada();
-   //criar uma variável que servira de indice
-   var i = 0;
-   //enquanto indice <=1000
-   //concatena na variável mensagem
-   while (i <= 10) {
-      var resultado = i * n;
-      mensagem += `${n} X ${i} = ${resultado} <br>`;
-      i++;
-   }
-   // mostrar mensagem no output
-   output.innerHTML = mensagem;
+function saida() {
+   var output = document.querySelector("#output");
+   output.innerHTML = n1.toFixed(2);
 }
 
-function limparTabuada() {
-   mensagem = "";
-   output.innerHTML = mensagem;
+function adi() {
+   var n = parseFloat(prompt("Digite outro número: "));
+   n1 += n;
+   saida();
 }
 
-// Tambémm consigo fazer isso com o metodo addEventListener
-// button.addEventListener("click", mostrarTabuada);
+function sub() {
+   var n = parseFloat(prompt("Digite outro número: "));
+   n1 -= n;
+   saida();
+}
+
+function mult() {
+   var n = parseFloat(prompt("Digite outro número: "));
+   n1 *= n;
+   saida();
+}
+
+function div() {
+   var n = parseFloat(prompt("Digite outro número: "));
+   n1 /= n;
+   saida();
+}

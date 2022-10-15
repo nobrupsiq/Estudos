70aa104a1a60eea093f7bbe23364ae900f451a5d
bruno <bpires.siqueira@gmail.com>
2022-10-15 17:27:09 -0300

    secao6-array

---
+++ logica-js/secao6-arrays/script.js
@@ -0,0 +1,9 @@
+var pessoa = 'maria';
+var pessoa2 = 'Joao';
+var pessoa3 = 'jose';
+
+var pessoas = new Array();
+pessoas[0] = 'jose'
+pessoas[1] = 'Maria'
+pessoas[2] = 'João'
+console.log(pessoas)

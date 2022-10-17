185ff88dde9a8e8332527418d320cc1872936f82
bruno <bpires.siqueira@gmail.com>
2022-10-17 17:48:15 -0300

    data-object

--- JavaScript/efeitos-no-dom/date-object/js/modules/funcionamento.js
+++ JavaScript/efeitos-no-dom/date-object/js/modules/funcionamento.js
@@ -1,14 +1,16 @@
-export default function initFuncionamento() {}
+export default function initFuncionamento() {
+    const funcionamento = document.querySelector("[data-semana]");
+    const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+    const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
 
-const agora = new Date();
-const futuro = new Date("Dec 24 2022");
+    const dataAgora = new Date();
+    const diaAgora = dataAgora.getDay();
+    const horarioAgora = dataAgora.getHours();
 
-console.log(agora.getMonth());
-console.log(futuro);
+    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+    const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
 
-//O método getTime() mostra o tempo total em milessegundos desde o dia 1 de janeiro de 1970.
-const mile = new Date();
-console.log(mile.getTime());
-
-const diasPassados = mile.getTime() / (24 * 60 * 60 * 1000);
-console.log(diasPassados);
+    if (semanaAberto && horarioAberto) {
+        funcionamento.classList.add("aberto");
+    }
+}
